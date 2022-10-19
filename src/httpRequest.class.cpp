87b43059c182ab5b359d33c9116a0de3acb288e9
anruland <anruland@student.42wolfsburg.de>
2022-10-19 14:09:11 +0200

    added location parsing of request

--- src/httpRequest.class.cpp
+++ src/httpRequest.class.cpp
@@ -6,7 +6,7 @@
 /*   By: anruland <anruland@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 16:49:53 by anruland          #+#    #+#             */
-/*   Updated: 2022/10/18 17:30:53 by anruland         ###   ########.fr       */
+/*   Updated: 2022/10/19 14:06:10 by anruland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ httpRequest::httpRequest(void)
 }
 httpRequest::httpRequest(std::string msg, httpConfig config)
 {
-	std::string	tempResource;
+	std::string	tempResourceDir = "/";
+	std::string	tempResourceFile;
 	if (DEBUG > 2)
 		std::cout << "httpConfig constructor with message" << std::endl;
 	std::cout << msg << std::endl;
@@ -28,11 +29,24 @@ httpRequest::httpRequest(std::string msg, httpConfig config)
 		throw std::logic_error("403");
 	this->mReqType = msg.substr(0, msg.find(" "));
 	this->mResource = msg.substr(msg.find(" ") + 1, msg.find(" ", msg.find(" ") + 1) - msg.find(" ") - 1);
-	std::vector<std::map<std::string, std::string> >::iterator it = config.getConfLocations().begin();
-	for (; it != config.getConfLocations().end(); it++)
+	if (this->mResource[this->mResource.size() - 1] != '/')
 	{
-		if (this->mResource[this->mResource.size() - 1] == '/')
-			std::cout << std::endl;
+		if (this->mResource.find_last_of(".") == this->mResource.npos)
+				this->mResource.append("/");
+		tempResourceFile = this->mResource.substr(this->mResource.find_last_of('/') + 1);
+		std::cout << "Temp Resource Dir is: " << tempResourceDir << std::endl;
+		std::cout << "Temp Resource File is: " << tempResourceFile << std::endl;
+	}
+	// APPEND '/' if no /  at the end of the path
+	tempResourceDir = this->mResource.substr(0, this->mResource.find_last_of('/') + 1);
+	std::vector<std::map<std::string, std::string> >::iterator itvec = config.getConfLocations().begin();
+	for (; itvec != config.getConfLocations().end(); itvec++)
+	{
+		if (!(*itvec)["location"].compare(tempResourceDir.c_str()))
+			std::cout << "location successful " << tempResourceDir << std::endl;
+		else
+			std::cout << "location unsuccessful " << tempResourceDir << std::endl;
+		// tempResourceFile = this->mResource.substr(this->mResource.find_last_of('/') + 1); FIND INDEX
 	}
 
 
